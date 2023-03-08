1b9d92e93d3a109b7cd235dcd07dddf6b9be3eb5
Lee Knoyer <lknoyer@Lees-MacBook-Pro.local>
2023-03-07 21:14:00 -0500

    added secret key and pipeline stage

--- main.tf
+++ main.tf
@@ -100,6 +100,13 @@ resource "aws_eip" "demo-eip" {
   ]
 }
 
+#### Key Pair ####
+resource "aws_key_pair" "deployer" {
+  key_name = "main-ssh-key"
+  # Just for testing purposes do not use as a real key for anything important or permanent.
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
+
 #### EC2 Instance ####
 resource "aws_instance" "demo-ec2" {
   ami               = "ami-0557a15b87f6559cf"
